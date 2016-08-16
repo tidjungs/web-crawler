fcad510297938e7995ec3f493701e6305133982c
tidjungs <waewprach.s@ku.th>
2016-08-16 22:08:54 +0700

    access google play & get next url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 const request = require('request')
 const cheerio = require('cheerio')
+const URL = 'https://play.google.com/store/apps/details?id=';
 
 server.connection({
 	host: 'localhost',
@@ -10,19 +11,43 @@ server.connection({
 
 server.route({
 	method: 'GET',
-	path: '/',
+	path: '/{appId}',
 	handler: (req, reply) => {
-		let url = 'http://devahoy.com/posts/scraping-web-with-nodejs'
+
+		let appId = req.params.appId;
+    	let lang = req.query.lang || 'en';
+    	let url = `${URL}${appId}&hl=${lang}`;
+    	console.log(url)
 		request(url, (err, response, body) => {
 
 			if(!err && response.statusCode === 200) {
+
 				let $ = cheerio.load(body)
-				let title = $('.title').text().trim()
+				let title = $('.document-title').text().trim()
+				let publisher = $('.document-subtitle.primary').text().trim()
+				let category = $('.document-subtitle.category').text().trim()
+		        let score = $('.score-container > .score').text().trim()
+		        let install = $('.meta-info > .content').eq(2).text().trim()
+		        let version = $('.meta-info > .content').eq(3).text().trim()
+
+		        //get next taget
+		        var list = [];
+					$('div.rec-cluster').find('div > div.cover > a').each(function (index, element) {
+					  list.push($(element).attr('href'));
+				});
+
 				reply({
-					title: title
+					title: title,
+		            publisher: publisher,
+		            category: category,
+		            score: score,
+		            install: install,
+		            version: version,
+		            list: list
 				})
 
 			} else {
+				console.log('error')
 				reply({
 					message: `error`
 				})
